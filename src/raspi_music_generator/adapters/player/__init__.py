49710e96c01ae9b2bf5aaf240227803dc58552e3
matti16 <bmattia92@gmail.com>
2021-01-06 16:54:07 +0100

    primer melody

--- src/raspi_music_generator/adapters/player/__init__.py
+++ src/raspi_music_generator/adapters/player/__init__.py
@@ -15,29 +15,20 @@ class Player():
     
     def play(self):
         self.played_songs = set()
-        current_songs = self._get_current_song_list()
-
-        pygame.mixer.music.load(current_songs[0])
+        pygame.mixer.music.load(self._get_current_song_list()[0])
         pygame.mixer.music.play()
-        self.played_songs.add(current_songs[0])
-        pygame.mixer.music.set_endevent(pygame.USEREVENT)
 
         running = True
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
-                elif event.type == pygame.USEREVENT:
-                    current_songs = self._get_current_song_list()
-                    songs_left = [c for c in current_songs if c not in self.played_songs]
-                    if len(songs_left):
-                        self.enqueue(songs_left[0])
 
 
     def enqueue(self, path):
         pygame.mixer.music.queue(path)
-        self.played_songs.add(path)
-    
+
+
     def stop(self):
         pygame.mixer.music.stop()
         pygame.mixer.music.unload()
